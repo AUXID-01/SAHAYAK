daf4e44731aef0ee304c26abfef5357c1dde5d88
AUXID-01 <ayushpatra240@gmail.com>
2025-11-03 18:43:40 +0530

    backend changed

--- backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login } from "../controllers/auth.controllers.js";
-
-const router = express.Router();
-
-// Login route (no authentication needed)
-router.post("/login", login);
-
-export default router;
